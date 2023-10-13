a6bc702fe0ecb8a338d08cdb10bc88456853b35a
Jose Moya <jose.moya@beyondpricing.com>
2023-10-13 13:19:45 -0600

    Add auto rollback

--- data_api/util/storage.py
+++ data_api/util/storage.py
@@ -1,6 +1,18 @@
-from sqlmodel import Session
+from sqlalchemy.exc import SQLAlchemyError
+from sqlmodel import Session as _Session
 
 from db.models import engine
+
+
+class Session(_Session):
+
+    def commit(self):
+        """Override the commit method to rollback on error."""
+        try:
+            super().commit()
+        except SQLAlchemyError as e:
+            self.rollback()
+            raise e
 
 
 def get_session():
